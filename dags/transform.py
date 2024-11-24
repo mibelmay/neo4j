28e0df589de418cb8b431106b494bdd71ae87c51
mibelmay <zemcovannastasia11@gmail.com>
2024-11-25 03:58:21 +0500

    transform users

--- dags/transform.py
+++ dags/transform.py
@@ -62,6 +62,41 @@ def transform_regions_data(bucket_name: str, object_name: str):
     # save_data_to_minio("temp", "Region_Name.csv", region_name_df)
 
 
+def transform_users_data(bucket_name: str, object_name: str):
+    csv_data = load_data_from_minio(bucket_name, object_name)
+    df = pd.read_csv(csv_data, index_col=False)
+
+    user_df = df[["userId"]].rename(columns={"userId": "user_id"})
+    user_df["load_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    user_username_df = df[["userId", "username"]].rename(columns={"userId": "user_id"})
+    user_username_df["load_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    user_birthdate_df = df[["userId", "birthdate"]].rename(
+        columns={"userId": "user_id"}
+    )
+    user_birthdate_df["load_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    user_regdate_df = df[["userId", "regDate"]].rename(
+        columns={"userId": "user_id", "regDate": "reg_date"}
+    )
+    user_regdate_df["load_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    genders = {"MALE": 1, "FEMALE": 2}
+
+    user_gender_df = df[["userId", "gender"]].rename(
+        columns={"userId": "user_id", "gender": "gender_id"}
+    )
+    user_gender_df["gender_id"] = user_gender_df["gender_id"].map(genders)
+    user_gender_df["load_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # save_data_to_minio("temp", "User.csv", user_df)
+    # save_data_to_minio("temp", "User_Username.csv", user_username_df)
+    # save_data_to_minio("temp", "User_Birthdate.csv", user_birthdate_df)
+    # save_data_to_minio("temp", "User_Reg_date.csv", user_regdate_df)
+    # save_data_to_minio("temp", "User_Gender.csv", user_gender_df)
+
+
 def save_data_to_minio(bucket_name: str, object_name: str, df: list):
     df.to_csv(object_name, index=False)
     minio_client.fput_object(bucket_name, object_name, object_name)
@@ -74,6 +109,16 @@ transform_regions_task = PythonOperator(
     op_args=[
         date.today().strftime("%Y-%m-%d"),
         f'Region-{date.today().strftime("%Y-%m-%d")}.csv',
+    ],
+    dag=dag,
+)
+
+transform_users_task = PythonOperator(
+    task_id="transform_users",
+    python_callable=transform_users_data,
+    op_args=[
+        date.today().strftime("%Y-%m-%d"),
+        f'User-{date.today().strftime("%Y-%m-%d")}.csv',
     ],
     dag=dag,
 )
